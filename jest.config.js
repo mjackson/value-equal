229575d1d7adcca3c84297500e4f7b3344f52d9d
Michael Jackson <michael@jackson.us>
2018-12-01 14:52:10 -0800

    Fix build

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+let mappedModule;
+switch (process.env.TEST_ENV) {
+  case 'cjs':
+    mappedModule = '<rootDir>/cjs/value-equal.js';
+    break;
+  case 'umd':
+    mappedModule = '<rootDir>/umd/value-equal.js';
+    break;
+  default:
+    mappedModule = '<rootDir>/modules/index.js';
+}
+
+module.exports = {
+  moduleNameMapper: {
+    '^value-equal$': mappedModule
+  },
+  testMatch: ['**/__tests__/**/*-test.js'],
+  testURL: 'http://localhost/'
+};
